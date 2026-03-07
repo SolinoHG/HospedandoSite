698cbf6fc45677e848e8bc37bb27369b83b32a69
Heguedusch <heguedusche@gmail.com>
2026-03-07 00:42:35 -0300

    Adicionando tela requisitos hardware

---
+++ game.js
@@ -0,0 +1,255 @@
+const jogos = {
+
+undertale: {
+titulo: "Undertale PT-BR",
+banner: "img/undertale.jpg",
+descricao: "Controle uma criança que caiu no subsolo cheio de monstros.",
+trailer: "",
+download: "#",
+specs: ["CPU: i3", "RAM: 4GB", "GPU: Integrada", "Armazenamento: 2GB"]
+},
+
+miside: {
+titulo: "Mi Side PT-BR",
+banner: "img/miside.jpg",
+descricao: "Uma história misteriosa dentro de um jogo estranho.",
+trailer: "",
+download: "#",
+specs: ["CPU: i3", "RAM: 8GB", "GPU: GTX 750", "Armazenamento: 10GB"]
+},
+
+crash4: {
+titulo: "Crash Bandicoot 4 PT-BR",
+banner: "img/crash4.jpg",
+descricao: "Crash e Coco precisam salvar o multiverso.",
+trailer: "",
+download: "#",
+specs: ["CPU: i5", "RAM: 8GB", "GPU: GTX 660", "Armazenamento: 30GB"]
+},
+
+forza3: {
+titulo: "Forza Horizon 3 Ultimate Edition",
+banner: "img/forza3.jpg",
+descricao: "Festival Horizon na Austrália com centenas de carros.",
+trailer: "",
+download: "#",
+specs: ["CPU: i5", "RAM: 8GB", "GPU: GTX 970", "Armazenamento: 55GB"]
+},
+
+silenthillf: {
+titulo: "Silent Hill F - Digital Deluxe Edition",
+banner: "img/shf.jpg",
+descricao: "Novo capítulo da famosa franquia de terror psicológico.",
+trailer: "",
+download: "#",
+specs: ["CPU: i7", "RAM: 16GB", "GPU: RTX 2060", "Armazenamento: 60GB"]
+},
+
+gowascension: {
+titulo: "God of War: Lore Ascension",
+banner: "img/gow.jpg",
+descricao: "Kratos enfrenta os deuses antes da saga original.",
+trailer: "",
+download: "#",
+specs: ["CPU: i5", "RAM: 8GB", "GPU: GTX 960", "Armazenamento: 40GB"]
+},
+
+hellblade2: {
+titulo: "Senua's Saga Hellblade II Enhanced",
+banner: "img/hellblade2.jpg",
+descricao: "Uma jornada sombria na mente de Senua.",
+trailer: "",
+download: "#",
+specs: ["CPU: i7", "RAM: 16GB", "GPU: RTX 2070", "Armazenamento: 70GB"]
+},
+
+indiana: {
+titulo: "Indiana Jones e o Grande Círculo",
+banner: "img/indiana.jpg",
+descricao: "Aventura épica com o famoso arqueólogo.",
+trailer: "",
+download: "#",
+specs: ["CPU: i7", "RAM: 16GB", "GPU: RTX 2060", "Armazenamento: 80GB"]
+},
+
+hellisus: {
+titulo: "Hell is Us Deluxe Edition",
+banner: "img/hellisus.jpg",
+descricao: "Exploração e combate em um mundo misterioso.",
+trailer: "",
+download: "#",
+specs: ["CPU: i7", "RAM: 16GB", "GPU: RTX 3060", "Armazenamento: 50GB"]
+},
+
+mw2019: {
+titulo: "Call of Duty Modern Warfare 2019",
+banner: "img/mw.jpg",
+descricao: "Guerra moderna intensa com campanha e multiplayer.",
+trailer: "",
+download: "#",
+specs: ["CPU: i5", "RAM: 12GB", "GPU: GTX 970", "Armazenamento: 175GB"]
+},
+
+usf4: {
+titulo: "Ultra Street Fighter IV",
+banner: "img/usf4.jpg",
+descricao: "Combate clássico com lutadores icônicos.",
+trailer: "",
+download: "#",
+specs: ["CPU: i3", "RAM: 4GB", "GPU: GTX 460", "Armazenamento: 12GB"]
+},
+
+deadspace: {
+titulo: "Trilogia Dead Space",
+banner: "img/deadspace.jpg",
+descricao: "Terror espacial enfrentando Necromorphs.",
+trailer: "",
+download: "#",
+specs: ["CPU: i5", "RAM: 8GB", "GPU: GTX 960", "Armazenamento: 60GB"]
+},
+
+nfs: {
+titulo: "Need for Speed Most Wanted",
+banner: "img/nfs.jpg",
+descricao: "Corridas ilegais e perseguições policiais.",
+trailer: "",
+download: "#",
+specs: ["CPU: i3", "RAM: 4GB", "GPU: GTX 460", "Armazenamento: 20GB"]
+},
+
+nioh3: {
+titulo: "Nioh 3 Digital Deluxe Edition",
+banner: "img/nioh3.jpg",
+descricao: "RPG de ação brutal inspirado no Japão feudal.",
+trailer: "",
+download: "#",
+specs: ["CPU: i7", "RAM: 16GB", "GPU: RTX 2070", "Armazenamento: 60GB"]
+},
+
+re4: {
+titulo: "Resident Evil 4 Gold Edition",
+banner: "img/re4.jpg",
+descricao: "Leon enfrenta uma vila infestada.",
+trailer: "",
+download: "#",
+specs: ["CPU: i5", "RAM: 8GB", "GPU: GTX 1050", "Armazenamento: 50GB"]
+},
+
+bf6: {
+titulo: "Battlefield 6 Phantom Edition",
+banner: "img/bf6.jpg",
+descricao: "Guerra em larga escala com batalhas massivas.",
+trailer: "",
+download: "#",
+specs: ["CPU: i7", "RAM: 16GB", "GPU: RTX 2060", "Armazenamento: 80GB"]
+},
+
+ninjagaiden4: {
+titulo: "Ninja Gaiden 4 Deluxe Edition",
+banner: "img/ninjagaiden4.jpg",
+descricao: "Combate ninja extremamente rápido.",
+trailer: "",
+download: "#",
+specs: ["CPU: i7", "RAM: 16GB", "GPU: RTX 3060", "Armazenamento: 60GB"]
+},
+
+acblackflag: {
+titulo: "Assassin's Creed Black Flag",
+banner: "img/acbf.jpg",
+descricao: "Aventuras piratas no Caribe.",
+trailer: "",
+download: "#",
+specs: ["CPU: i5", "RAM: 8GB", "GPU: GTX 760", "Armazenamento: 30GB"]
+},
+
+darksouls2: {
+titulo: "Dark Souls II Scholar of the First Sin",
+banner: "img/ds2.jpg",
+descricao: "Desafios brutais em um mundo sombrio.",
+trailer: "",
+download: "#",
+specs: ["CPU: i3", "RAM: 4GB", "GPU: GTX 465", "Armazenamento: 25GB"]
+},
+
+gotham: {
+titulo: "Gotham Knights",
+banner: "img/gotham.jpg",
+descricao: "Proteja Gotham após a queda do Batman.",
+trailer: "",
+download: "#",
+specs: ["CPU: i7", "RAM: 16GB", "GPU: RTX 2070", "Armazenamento: 50GB"]
+},
+
+injustice2: {
+titulo: "Injustice 2 Legendary Edition",
+banner: "img/injustice2.jpg",
+descricao: "Heróis e vilões da DC em combates épicos.",
+trailer: "",
+download: "#",
+specs: ["CPU: i5", "RAM: 8GB", "GPU: GTX 970", "Armazenamento: 40GB"]
+},
+
+spiderman: {
+titulo: "Marvel's Spider-Man Remastered",
+banner: "img/spiderman.jpg",
+descricao: "Balance-se por Nova York como o Homem-Aranha.",
+trailer: "",
+download: "#",
+specs: ["CPU: i7", "RAM: 16GB", "GPU: RTX 2060", "Armazenamento: 75GB"]
+},
+
+xenoverse2: {
+titulo: "Dragon Ball Xenoverse 2",
+banner: "img/xeno2.jpg",
+descricao: "Crie seu lutador e lute na história de Dragon Ball.",
+trailer: "",
+download: "#",
+specs: ["CPU: i5", "RAM: 8GB", "GPU: GTX 750", "Armazenamento: 20GB"]
+},
+
+rdr2: {
+titulo: "Red Dead Redemption 2 Ultimate Edition",
+banner: "img/rdr2.jpg",
+descricao: "A épica história de Arthur Morgan no Velho Oeste.",
+trailer: "",
+download: "#",
+specs: ["CPU: i7", "RAM: 16GB", "GPU: GTX 1060", "Armazenamento: 150GB"]
+},
+
+evilwithin2: {
+titulo: "The Evil Within 2",
+banner: "img/evilwithin2.jpg",
+descricao: "Terror psicológico intenso.",
+trailer: "",
+download: "#",
+specs: ["CPU: i5", "RAM: 8GB", "GPU: GTX 960", "Armazenamento: 40GB"]
+},
+
+mafia: {
+titulo: "Mafia Definitive Edition",
+banner: "img/mafia.jpg",
+descricao: "Remake do clássico de máfia.",
+trailer: "",
+download: "#",
+specs: ["CPU: i5", "RAM: 8GB", "GPU: GTX 970", "Armazenamento: 50GB"]
+},
+
+naruto: {
+titulo: "Naruto Shippuden Ultimate Ninja Storm 4",
+banner: "img/naruto.jpg",
+descricao: "Batalhas épicas do anime Naruto.",
+trailer: "",
+download: "#",
+specs: ["CPU: i3", "RAM: 4GB", "GPU: GTX 660", "Armazenamento: 40GB"]
+},
+
+tlou2: {
+titulo: "The Last of Us Parte II Remastered",
+banner: "img/tlou2.jpg",
+descricao: "A jornada brutal de Ellie.",
+trailer: "",
+download: "#",
+specs: ["CPU: i7", "RAM: 16GB", "GPU: RTX 2070", "Armazenamento: 100GB"]
+}
+
+}
